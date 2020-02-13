620ec8f19614ad76b242aac992c8e9302430660e
Ajay Prasad <ajay.prasad-ctr@digitalreasoning.com>
2020-02-13 15:33:21 +0530

    setting up for heroku deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast=require('./utils/forecast')
 
 const app = express();
 
+const port=process.env.PORT || 3000;
 // define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -92,6 +93,6 @@ app.get("*", (req, res) => {
 });
 
 // create server
-app.listen(3000, () => {
-  console.log("Server started with the port 3000");
+app.listen(port, () => {
+  console.log("Server started with the port",port);
 });
